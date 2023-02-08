df6dab859b58e00f58aa2d0740b3c468c9bf08e9
Donny Pratama <donnypratama1024@gmail.com>
2023-02-08 14:45:37 +0700

    refactor: make container as component

--- src/views/RestoDetailView.vue
+++ src/views/RestoDetailView.vue
@@ -2,6 +2,8 @@
 import { useRestoRepository } from "@/composables";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import BaseCard from "@/components/BaseCard.vue";
+import BaseContainer from "../components/BaseContainer.vue";
 
 const repository = useRestoRepository();
 const route = useRoute();
@@ -25,5 +27,9 @@ onMounted(() => fetchDetail());
 </script>
 
 <template>
-  <div>Sedang menampilkan resto dengan ID: {{ route.params.id }}</div>
+  <BaseContainer>
+    <BaseCard
+      >Sedang menampilkan resto dengan ID: {{ route.params.id }}</BaseCard
+    >
+  </BaseContainer>
 </template>
